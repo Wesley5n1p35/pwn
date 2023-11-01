b62dfd136d6b976438e576d1dd0f4125ba19c0b2
Wesley5n1p35 <47337218+Wesley5n1p35@users.noreply.github.com>
2023-11-01 12:41:44 -0500

    Update all.ps1

--- all.ps1
+++ all.ps1
@@ -1,4 +1,3 @@
-                                                                                                                                                                                                                                           
 $i = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);';
 add-type -name win -member $i -namespace native;
 [native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0);
